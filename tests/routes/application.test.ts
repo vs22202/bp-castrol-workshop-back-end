7f62865b1e7f5964ebadfc5fec681e4a9feeea24
Vishaal <vishaal.s2020@vitstudent.ac.in>
2024-02-11 09:21:01 +0530

    Cleaned Up Test

--- tests/routes/application.test.ts
+++ tests/routes/application.test.ts
@@ -4,7 +4,6 @@ import { Application } from "../../src/models/application";
 describe("Application API", () => {
     it('GET /application', async () => {
         const response = await request(app).get("/application");
-        console.log(response.body[0]);
         const temp: Application = {
             Application_Id: expect.any(Number),
             Workshop_Name: expect.any(String),
