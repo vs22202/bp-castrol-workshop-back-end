30d73d359f2bde80a812bbb03ab3af6cd28c8be9
Vishaal <vishaal.s2020@vitstudent.ac.in>
2024-03-14 02:01:43 +0530

    Added Change + Reset Password + route to Get user data using user_id

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ app.use('/application', require('./routes/application').default);
 app.use('/register', require('./routes/register').default);
 app.use('/login', require('./routes/login').default);
 app.use('/generateOtp', require('./routes/generateOtp').default);
+app.use('/user', require('./routes/user').default);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Application started');
