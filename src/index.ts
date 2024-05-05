1ecd1f8bb00a1ced459a1b0db91e5221bc897a84
Vishaaal Sowrirajan <71444932+vs22202@users.noreply.github.com>
2024-05-05 12:52:51 +0530

    Revert "Logger"

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { initializeDB } from './db';
 import express, { Request, Response } from 'express';
 import cors from "cors";
-import {errorMorgan, infoMorgan} from "./logger/morgan-middleware";
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(errorMorgan);
-app.use(infoMorgan);
+
 //  Routes
 app.use('/application', require('./routes/application').default);
 app.use('/register', require('./routes/register').default);
